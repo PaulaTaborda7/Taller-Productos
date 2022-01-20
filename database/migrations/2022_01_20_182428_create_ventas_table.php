6d487769b48573ec1c5821d5aa9b46f090ff1b9f
jupachon <jupachon@unal.edu.co>
2022-01-20 13:34:43 -0500

    Creo modelo, migración y controlador de Ventas

---
+++ database/migrations/2022_01_20_182428_create_ventas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVentasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ventas', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('idCliente');
+            $table->unsignedBigInteger('idProducto');
+            $table->foreign('idCliente')->references('id')->on('clientes');
+            $table->foreign('idProducto')->references('producto_id')->on('productos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ventas');
+    }
+}
