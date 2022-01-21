aed62e1cf919f15fd2b41ae070ceb65884176a9c
Almunozr <almunzr@unal.edu.co>
2022-01-21 15:00:45 -0500

    index de ventas

---
+++ resources/views/ventas/index.blade.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+    @include("partes.head")
+<body>
+    <div class="container text-center">
+
+        <h1>Ventas: - {{ count($ventas) }}</h1>
+
+
+        <table id="example" class="table table-striped" style="width:100%">
+            <thead>
+                <tr>
+                    <th>N.Venta</th>
+                    <th>Producto</th>
+                    <th>Cliente</th>
+
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($ventas as $venta)
+                    <tr>
+                        <td>{{ $loop->iteration }}</td>
+                        <td>
+                            @php
+                                use App\Models\Cliente;
+                                $cliente = Cliente::where('id',$venta->idCliente)->first()
+                            echo $producto->nombre
+                            @endphp
+                        </td>
+                        <td>
+                            @php
+                                use App\Models\Productos;
+                                $producto = Productos::where('id', $venta->idProducto)->first()
+                            @endphp
+                        </td>
+
+
+                            {{-- <a href="{{ route('productos.edit', $producto->producto_id) }}">
+                                <button class="btn btn-primary">
+                                    Editar
+                                </button>
+                            </a> --}}
+                            {{-- Formulario de borrado --}}
+                            {{-- <form action="{{ route('productos.destroy',$producto->producto_id) }}" method="post">
+                                @method('DELETE')
+                                @csrf
+                                <input class="btn btn-danger" type="submit" onclick="return confirm('¿Quieres borrar?')" value="Borrar">
+                            </form> --}}
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+            <tfoot>
+                <tr>
+                    <th>N.Venta</th>
+                    <th>Producto</th>
+                    <th>Cliente</th>
+                </tr>
+            </tfoot>
+        </table>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+		integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+		crossorigin="anonymous"></script>
+	<script src="api.js"></script>
+	<script src="script.js"></script>
+
+	<!-- Información de los data tables obtenida de: https://datatables.net/extensions/fixedheader/examples/integration/responsive-bootstrap.html -->
+	<!-- Scripts necesarios para las data tables -->
+	<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+	<script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
+	<script src="https://cdn.datatables.net/1.11.3/js/dataTables.bootstrap5.min.js"></script>
+	<script src="https://cdn.datatables.net/1.11.3/js/dataTables.bootstrap.min.js"></script>
+	<!-- Scripts para que sea responsive -->
+	<script src="https://cdn.datatables.net/fixedheader/3.2.0/js/dataTables.fixedHeader.min.js"></script>
+	<script src="https://cdn.datatables.net/responsive/2.2.9/js/dataTables.responsive.min.js"></script>
+	<script src="https://cdn.datatables.net/responsive/2.2.9/js/responsive.bootstrap.min.js"></script>
+	<script>
+		$(document).ready(function () {
+			var table = $('#example').DataTable({
+				responsive: true
+			});
+			new $.fn.dataTable.FixedHeader(table);
+		});
+	</script>
+</body>
+</html>
